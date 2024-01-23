86918b042c89ff9381d1681baab651da835b6608
DD <ddonegal1@gmail.com>
2024-01-23 16:03:19 -0500

    added a remove box function

--- src/Box.js
+++ src/Box.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-function Box({ width = 500, height= 500,backgroundColor='red', onRemove }) {
+function Box({ width = 500, height = 500, backgroundColor = 'red', onRemove }) {
     const boxStyle = {
         width,
         height,
         backgroundColor
     }
-return(
-    <div style={boxStyle}>
-
-    </div>
-)
+    return (
+        <div style={boxStyle}>
+            <button onClick={onRemove}>X</button>
+        </div>
+    )
 }
 
 export default Box;
--- src/BoxList.js
+++ src/BoxList.js
@@ -10,12 +10,21 @@ function BoxList() {
     setBoxes((prevBoxes) => [...prevBoxes, newBox]);
  };
 
+ const removeBox=(index) => {
+    setBoxes((prevBoxes) => {
+        const newBoxes = [...prevBoxes];
+        newBoxes.splice(index,1);
+        return newBoxes
+    });
+ };
+
 return(
     <div>
         <Box
         width={Box.width}
         height={Box.height}
         backgroundColor={Box.backgroundColor}
+        onRemove={removeBox}
          />
     </div>
 )
